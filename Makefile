ea657bba43fee473da0150d817e0d4229d8a4e04
Matteo Riondato <matteo_riondato@brown.edu>
2014-06-22 12:39:01 -0400

    Initial import

---
+++ Makefile
@@ -0,0 +1,32 @@
+HFILES1 = glob.h dist.h
+HFILES2 = gen.h
+
+CFILES1 =  ran1.c expdev.c gammln.c gasdev.c poidev.c dist.c
+CFILES2 =  gen.c main.c command.c
+
+OBJECTS1 = ran1.o expdev.o gammln.o gasdev.o poidev.o dist.o
+OBJECTS2 = gen.o main.o command.o
+
+LIBES = -lm
+CC = g++
+#CFLAGS = -+ -O
+
+EXEC = gen 
+
+default: $(EXEC)
+
+gen:	$(OBJECTS1) $(OBJECTS2)
+	$(CC) $(CFLAGS) $(OBJECTS1) $(OBJECTS2) $(LIBES) -o gen
+
+test:	$(OBJECTS1) test.o
+	$(CC) $(CFLAGS) $(OBJECTS1) test.o $(LIBES) -o test
+
+mrproper:
+	/bin/rm -f $(OBJECTS1) $(OBJECTS2) $(EXEC)
+
+clean:
+	/bin/rm -f $(OBJECTS1) $(OBJECTS2)
+
+$(OBJECTS1): $(HFILES1)
+$(OBJECTS2): $(HFILES1) $(HFILES2)
+
---
+++ run.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# a simple script to drive the generation of synthetic datasets
+
+
+# Command Line Options:
+#   -ntrans number_of_transactions_in_000s (default: 1000)
+#   -tlen avg_items_per_transaction (default: 10)
+#   -nitems number_of_different_items_in_000s) (default: 100)
+# 
+#   -npats number_of_patterns (default: 10000)
+#   -patlen avg_length_of_maximal_pattern (default: 4)
+#   -corr correlation_between_patterns (default: 0.25)
+#   -conf avg_confidence_in_a_rule (default: 0.75)
+# 
+#   -fname <filename> (write to filename.data and filename.pat)
+#   -ascii (default: False)
+#   -randseed # (reset seed used generate to x-acts; must be negative)
+#   -version (to print out version info)
+
+L_TLEN="10"                    # T
+L_NTRANS="20"               # D
+L_NITEMS="1"                   # N
+
+NPATS=2000                     # L 
+PATLEN=8                      # I
+CORR=0.25
+CONF=0.75
+ASCII=
+RANDSEED=
+#RANDSEED=-123456789
+
+OUTPATH="."
+#--------------------------------------------------------------------
+
+for NTRANS in ${L_NTRANS}; do 
+  for TLEN in ${L_TLEN}; do
+    for NITEMS in ${L_NITEMS}; do
+
+      FNAME="$OUTPATH/data_${NTRANS}k_t${TLEN}_p${PATLEN}_l${NPATS}_n${NITEMS}k"
+      OPT="-ntrans ${NTRANS} -tlen ${TLEN} -nitems ${NITEMS} -npats ${NPATS}"
+      OPT="${OPT} -patlen ${PATLEN} -corr ${CORR} -conf ${CONF}"
+      OPT="${OPT} -fname ${FNAME} ${ASCII}"
+
+      comm="./gen lit $OPT"
+
+      echo $comm
+      $comm
+
+    done
+  done
+done
+
